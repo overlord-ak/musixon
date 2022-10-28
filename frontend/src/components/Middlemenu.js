2ea6107618aa65c26c903217535548b02ab2b4b6
Anyasha Das <81379481+anyasha08@users.noreply.github.com>
2022-10-29 02:37:57 +0530

    files updated

---
+++ frontend/src/components/Middlemenu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Middlemenu() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export { Middlemenu };
---
+++ frontend/src/components/Rightmenu.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Rightmenu() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export { Rightmenu };
